87ebb23f4f26ccd4049db03cd5cb2c84ee011c3c
git <harnengh@gmail.com>
2015-10-24 12:30:33 +0200

    Renamed chains to look better in the gui. Built a gui (not styled, mind).

--- static/script.js
+++ static/script.js
@@ -27,6 +27,44 @@ function showOnlyQuestion() {
 	show("the_question");
 }
 
+function clearOptions(selectElement) {
+	while (selectElement.length > 0) {
+		selectElement.remove(0);
+	}
+}
+
+function addOption(selectElement, new_option) {
+	var option = document.createElement("option");
+	option.text = new_option;
+	option.value = new_option;
+	selectElement.add(option);
+}
+
+function selectedLanguageChanged() {
+	var selected_language = document.getElementById("language_select").value;
+	var category_select = document.getElementById("category_select");
+	clearOptions(category_select);
+
+	var categories = OPTIONS.options.languages[selected_language].categories;
+	for (var cat in categories) {
+		addOption(category_select, cat);
+	}
+}
+
+function optionsLoaded() {
+	var langs = OPTIONS.options.languages;
+
+	var language_select = document.getElementById("language_select");
+	clearOptions(language_select);
+
+	for (var language in langs) {
+		addOption(language_select, language);
+	}
+
+	selectedLanguageChanged();
+	showOnlyQuestion();
+}
+
 var OPTIONS = (function () {
 	var mod = {};
 
